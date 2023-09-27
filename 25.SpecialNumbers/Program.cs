c242c4ad7befb2d31f6e6c93ed81b7e577cf2add
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-09-27 23:36:52 +0300

    special numbers

---
+++ 25.SpecialNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _25.SpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine() ?? "1");
+
+
+            for (int i = 1111; i <= 9999; i++)
+            {
+                string currentString = i.ToString();
+                bool isSpecial = true;
+
+                for (int curNum = 0; curNum < currentString.Length; curNum++)
+                {
+                    int curNumValue = int.Parse(currentString[curNum].ToString());
+
+                    if (curNumValue == 0 || number % curNumValue != 0)
+                    {
+                        isSpecial = false;
+                        break;
+                    }
+                }
+                if (isSpecial)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
---
+++ stringReadin/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace stringReadin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string textToRead = Console.ReadLine();
+
+            for (int i = 0; i <= textToRead.Length - 1; i ++)
+            {
+                Console.WriteLine(textToRead[i]);
+            }
+        }
+    }
+}
