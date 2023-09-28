90e39ad90629bd7f2776d96fb456efd414f46d0a
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-09-28 23:15:35 +0300

    more simpletons

---
+++ 008.TicketPrice/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _008.TicketPrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ticketType = Console.ReadLine();
+
+            switch (ticketType)
+            {
+                case "student": Console.WriteLine("$1.00"); break;
+                case "regular": Console.WriteLine("$1.60"); break;
+                default: Console.WriteLine("Invalid ticket type!"); break;
+            }
+        }
+    }
+}
---
+++ 009.AreaOfFigures/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _009.AreaOfFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figureType = Console.ReadLine();
+
+            switch (figureType)
+            {
+                case "square":
+                    double side = double.Parse(Console.ReadLine());
+                    double area = side * side;
+                    Console.WriteLine($"{area:F2}");
+                    break;
+                case "rectangle":
+                    double width = double.Parse(Console.ReadLine());
+                    double length = double.Parse(Console.ReadLine());
+                    area = width * length;
+                    Console.WriteLine($"{area:F2}");
+                    break;
+                case "circle":
+                    double radius = double.Parse(Console.ReadLine());
+                    area = Math.PI * radius * radius;
+                    Console.WriteLine($"{area:F2}");
+                    break;
+                default: Console.WriteLine("Invalid figure."); break;
+            }
+        }
+    }
+}
