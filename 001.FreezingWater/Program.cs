71b561f96c7b54bdb8dada44712e1389fb5bb317
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-09-28 22:04:33 +0300

    simple starts over

---
+++ 001.FreezingWater/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _001.FreezingWater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double temp = double.Parse(Console.ReadLine());
+
+            if (temp < 0)
+            {
+                Console.WriteLine("Freezing weather!");
+            }
+        }
+    }
+}
