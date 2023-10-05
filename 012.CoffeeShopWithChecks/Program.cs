6e7933ae80b67c6fa34b7fe2f54f1000c014d3f4
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-10-05 09:29:03 +0300

    small repetition

---
+++ 012.CoffeeShopWithChecks/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _012.CoffeeShopWithChecks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string orderedDrink = Console.ReadLine() ?? "tea";
+            string orderedExtra = Console.ReadLine() ?? "no";
+
+            double finalPrice = 0.0;
+            bool validOrder = true;
+
+            if (orderedDrink == "tea")
+                finalPrice += 0.60;
+            else if (orderedDrink == "coffee")
+                finalPrice += 1.00;
+            else
+            {
+                Console.WriteLine("Unknown drink");
+                validOrder = false;
+            }
+
+
+            if (validOrder)
+            {
+                if (orderedExtra == "sugar")
+                    finalPrice += 0.40;
+                else if (orderedExtra == "no")
+                    finalPrice += 0;
+                else
+                {
+                    validOrder = false;
+                    Console.WriteLine("Unknown extra");
+                }
+            }
+
+            if (validOrder == true)
+                Console.WriteLine($"Final price: ${finalPrice:f2}");
+
+        }
+    }
+}
